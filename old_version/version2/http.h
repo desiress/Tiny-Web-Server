1a2f8cb5d429451ef376ca3089ac5e6b65554f4f
desiress <125067068@qq.com.com>
2020-03-12 23:06:08 +0800

    add threadpool

---
+++ old_version/version2/http.h
@@ -0,0 +1,10 @@
+#ifndef _HTTP_H
+#define _HTTP_H
+
+void *read_and_respond(void *arg);
+void handle_request(const char* request, int cfd);
+void send_head(int cfd, int no, const char* desp, const char* type, long len);
+void send_file(int cfd, const char* filename);
+void send_dir(int cfd, const char* dirname);
+
+#endif
